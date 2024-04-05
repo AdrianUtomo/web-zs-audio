4b3c7a1eef6245717f338f34e6f5a9e890ab8490
Adrian <adrianhakimutomo@gmail.com>
2024-04-05 13:42:53 +0700

    chores: change font size and weight

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="flex flex-col justify-center items-center w-[70%] text-center">
+    <div className="flex flex-col justify-center items-center w-[70%] text-center text-[#05629b]">
       <p className="font-bold text-4xl">VOICE DEMO:</p>
-      <p className="text-2xl">
+      <p className="font-bold text-[2rem]">
         Zero-Shot Voice Cloning Text-to-Speech for Dysphonia Disorder Speakers
       </p>
     </div>
