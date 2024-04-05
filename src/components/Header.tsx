6a6de142905fd06343719bf4b61f5d0ea7858ffb
Adrian <adrianhakimutomo@gmail.com>
2024-04-05 14:16:16 +0700

    chores: change font size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="flex flex-col justify-center items-center w-[70%] text-center text-[#05629b]">
       <p className="font-bold text-4xl">VOICE DEMO:</p>
-      <p className="font-bold text-[2rem]">
+      <p className="font-bold text-[1.8rem]">
         Zero-Shot Voice Cloning Text-to-Speech for Dysphonia Disorder Speakers
       </p>
     </div>
